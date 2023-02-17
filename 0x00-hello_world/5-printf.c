a07e613fd0e87d2f3b790248c87a43d43f56f165
kassem00 <kamsdonga@gmail.com>
2023-02-17 23:46:58 +0200

    1

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,9 @@
-#include<stdio.h>
-int main(){
-puts("\"Programming is like building a multilingual puzzle");
-return 0;
-}
+#include <stdio.h>
+/**
+ * Description: main-funcation {print text}
+ * Return: 0 if it success.
+ */
+int main(void)
+{
+printf("\"Programming is like building a multilingual puzzle");
+return (0); }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,9 @@
-#!/bin/bash
-gcc -E $CFILE  -o  c
+#include <stdio.h>
+/**
+ * Description: main-funcation {print text}
+ * Return: 0 if it success.
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0); }
