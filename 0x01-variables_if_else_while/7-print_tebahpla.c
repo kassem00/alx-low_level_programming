6d03b2e4d4191655011561f8ce849f17c3cbb74b
kassem00 <kamsdonga@gmail.com>
2023-02-19 09:10:00 +0200

    1

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,13 +2,14 @@
 #include <time.h>
 #include <stdio.h>
 /**
-* main - program that prints the lowercase alphabet in reverse, followed by a new line.
+* main - program that prints the lowercase alphabet in reverse,
+* followed by a new line.
 * Return: 0
 */
 int main(void)
 {
 char alpha = 122;
-while (96 < alpha)
+while (alpha > 96)
 {
 putchar((char)alpha);
 alpha--;
