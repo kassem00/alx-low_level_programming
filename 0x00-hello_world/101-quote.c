d732383623e0fa4179001d049b0a5b61d338e8db
kassem00 <kamsdonga@gmail.com>
2023-02-20 00:47:09 +0200

    1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: 0
  */
 int main(void)
 {
-char the_text[80]= " and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char the_text[80]= "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 puts(the_text);
 return (1);
 }
