3c548a7ca759881fae76bb79ea186dd545c0fb21
kassem00 <kamsdonga@gmail.com>
2023-02-21 21:32:53 +0200

    1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - assign a random number to the variable n each time it is executed
+* if the number is greater than 0: is positive
+* if the number is 0: is zero
+* if the number is less than 0: is negative
+* Return: 0
+*/
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
