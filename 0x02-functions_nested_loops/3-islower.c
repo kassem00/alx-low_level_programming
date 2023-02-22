4c270da083fcf85673b57366ed4e85d682225f79
kassem00 <kamsdonga@gmail.com>
2023-02-22 05:50:36 +0200

    1

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _islower - function that is lowercase?
+* followed by a new line.
+* main - program that prints the alphabet in lowercase, followed by a new line.
+* Return: 0
+*/
+int _islower(int c){
+int alpha = 97;
+if (alpha < 97){
+return (0);
+}
+else
+{
+return (1);
+}
+}
