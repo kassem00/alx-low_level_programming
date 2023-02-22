00e499d575d8ceb654e2bc916659e4894f5f3f6a
kassem00 <kamsdonga@gmail.com>
2023-02-22 07:04:47 +0200

    1

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,25 +1,26 @@
 #include "main.h"
 /**
 * print_sign - function that is lowercase?
-* @c: The input character
+* @n: The input character
 * main - program that prints the alphabet in lowercase, followed by a new line.
 * Return: 0
 */
 int print_sign(int n)
 {
-if (c > 0)
+if (n > 0)
 {
 return (1);
 _putchar('1');
 }
-if (c == 0)
+if (n == 0)
 {
 return (0);
 _putchar('0');
 }
-if (c < 0)
+if (n < 0)
 {
 return (-1);
-_putchar("-1");
+_putchar((char)45);
+_putchar((char)49);
 }
 }
