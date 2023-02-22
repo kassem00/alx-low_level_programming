7d7051bc6d81eb1d1056b1e399e9eda38dc51bfa
kassem00 <kamsdonga@gmail.com>
2023-02-22 23:08:08 +0200

    1

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* print_sign - function that is lowercase?
+* @n: The input character
+* main - program that prints the alphabet in lowercase, followed by a new line.
+* Return: 0
+*/
+int _abs(int x)
+{
+if (x < 0)
+x = x * -1;
+return (x);
+}
