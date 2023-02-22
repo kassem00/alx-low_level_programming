47c3a5eca60b3e652d4a57d34b14f55772deb289
kassem00 <kamsdonga@gmail.com>
2023-02-22 03:39:55 +0200

    1

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* print_alphabet - function that prints the alphabet, in lowercase,
+* followed by a new line.
+* main - program that prints the alphabet in lowercase, followed by a new line.
+* Return: 0
+*/
+void print_alphabet_x10(void){
+int turns=10;
+while(turns < 10)
+{
+char alpha = 97;
+while (alpha <= 122)
+{
+_putchar((char)alpha);
+alpha++;
+}
+_putchar('\n');
+}
+}
