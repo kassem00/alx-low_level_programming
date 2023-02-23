2c402e7f5b3379c6236d4b9174029296384ca275
kassem00 <kamsdonga@gmail.com>
2023-02-23 02:51:50 +0200

    1

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*  print_last_digit_abs - function that is lowercase?
+* @x: The input value.
+* Return: x
+*/
+void jack_bauer(void)
+{
+int hu_f_dig=0,hu_s_dig=0,mi_f_dig=0,mi_s_dig=0;
+while (mi_s_dig != 2 && hu_f_dig != 3 && mi_s_dig != 5 && mi_f_dig != 9)
+{
+_putchar(hu_f_dig,hu_s_dig,mi_f_dig,mi_s_dig);
+}
+return (x);
+}
