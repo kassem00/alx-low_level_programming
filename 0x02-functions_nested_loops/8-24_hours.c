00fb23058634cf5d5f6670361eed7a1be05b0840
kassem00 <kamsdonga@gmail.com>
2023-02-23 04:05:35 +0200

    1

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,9 +7,46 @@
 void jack_bauer(void)
 {
 int hu_f_dig=0,hu_s_dig=0,mi_f_dig=0,mi_s_dig=0;
-while (mi_s_dig != 2 && hu_f_dig != 3 && mi_s_dig != 5 && mi_f_dig != 9)
+while (hu_s_dig <= 2 && hu_f_dig <= 3 && mi_s_dig <= 5 && mi_f_dig <= 9)
 {
-_putchar(hu_f_dig,hu_s_dig,mi_f_dig,mi_s_dig);
+_putchar(hu_s_dig+ '0');
+_putchar(hu_f_dig+ '0');
+_putchar(':');
+_putchar(mi_s_dig+ '0');
+_putchar(mi_f_dig+ '0');
+_putchar('\n');
+
+if(mi_f_dig ==9)
+{
+if (mi_s_dig == 5)
+{
+if (hu_f_dig == 3){
+hu_s_dig++;
+hu_f_dig=0;
+mi_s_dig = 0;
+mi_f_dig = 0;
+}else{
+mi_s_dig = 0;
+mi_f_dig = 0;
+hu_f_dig++;
 }
-return (x);
 }
+else
+{
+mi_s_dig++;
+mi_f_dig = 0;
+}
+}
+else
+{
+mi_f_dig++;
+}
+}
+}
+/**
+ 
+ */
+
+/*
+
+*/
