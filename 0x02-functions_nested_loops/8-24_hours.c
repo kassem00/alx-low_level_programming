fdbd689a613cb64990f6ea59d704fe78453741f7
kassem00 <kamsdonga@gmail.com>
2023-02-23 05:36:23 +0200

    1

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -20,12 +20,15 @@ if(mi_f_dig == 9)
 {
 if (mi_s_dig == 5)
 {
-if (hu_f_dig == 9){
+if (hu_f_dig == 9)
+{
 hu_s_dig++;
 hu_f_dig = 0;
 mi_s_dig = 0;
 mi_f_dig = 0;
-}else{
+}
+else
+{
 if(hu_f_dig == 3 && hu_s_dig == 2)
 break;
 hu_f_dig++;
