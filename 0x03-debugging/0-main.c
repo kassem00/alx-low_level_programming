d3b7c1395b2e7afd7fe952ec7061f5264a14c46c
kassem00 <kamsdonga@gmail.com>
2023-02-24 02:07:06 +0200

    1

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,16 @@
 #include "main.h"
+
 /**
-* main - assign a random number to the variable n each time it is executed
-* if the number is greater than 0: is positive
-* if the number is 0: is zero
-* if the number is less than 0: is negative
-* Return: 0
-*/
+ * main - Test function for positive or negative
+ * Return: 0
+ */
+
 int main(void)
 {
-int n = 0;
-positive_or_negative(n);
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
 return (0);
 }
