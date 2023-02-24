63d0fe38831ff660cc5570be01f8422555f610ea
kassem00 <kamsdonga@gmail.com>
2023-02-24 06:24:50 +0200

    1

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,16 +6,16 @@
  */
 void print_line(int n)
 {
-int i = 0;
-while (i < n)
+if (n <= 0)
 {
-if (n > 0)
+_putchar('\n');
+} else
 {
-_putchar(95);
-i++;		
+int i;
+for (i = 1; i <= n; i++)
+{
+_putchar('_');
 }
-else
 _putchar('\n');
 }
-_putchar('\n');
 }
