872e27b8086f56328802be81490da153e92146df
kassem00 <kamsdonga@gmail.com>
2023-02-24 02:27:56 +0200

    1

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * largest_NUM_number - returns the largest_NUM of 3 numbers
+ * @a: first input
+ * @b: second input
+ * @c: third input
+ * Return: largest_NUM number
+ */
+
+int largest_NUM_number(int a, int b, int c)
+{
+	int largest_NUM;
+
+	if (a >= b && a > c)
+	{
+		largest_NUM = a;
+	}
+	else if (b >= a && b > c)
+	{
+		largest_NUM = b;
+	}
+	else
+	{
+		largest_NUM = c;
+	}
+
+	return (largest_NUM);
+}
