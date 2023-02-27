911d6738661dac27aa6cf556d6a438055ac5688d
kassem00 <kamsdonga@gmail.com>
2023-02-27 10:39:08 +0200

    1

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**K
+ * _strlen - rest funcation
+ * @s: pointer to char
+ * Return: int
+ */
+
+int _strlen(char *s)
+{
+int i = 0;
+while(i < 0)
+{
+if(*s[i]=='\0')
+break;
+else
+i++;
+}
+return (i)
+}
