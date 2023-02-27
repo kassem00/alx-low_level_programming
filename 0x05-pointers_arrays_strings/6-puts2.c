04c69defdcb7c6333bdb55509b74f27e6bedbe53
kassem00 <kamsdonga@gmail.com>
2023-02-27 15:54:45 +0200

    1

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,14 +1,25 @@
 #include "main.h"
 #include <stdio.h>
 #include <string.h>
+#include "holberton.h"
+
 /**
- * puts2 -  function that prints a string,
- * in reverse, followed by a new line.
+ * puts2 - function that prints every other character of a string
  * @str: pointer to char
- * Return: void
+ * Return: nothing
  */
 
 void puts2(char *str)
 {
-
+int j = 0,temp;
+while (str[j] != '\0')
+{
+temp=j % 2;
+if (temp == 0)
+{
+_putchar(*(str + j));
+}
+j++;
+}
+_putchar('\n');
 }
