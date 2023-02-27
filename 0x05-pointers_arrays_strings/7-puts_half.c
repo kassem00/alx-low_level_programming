6dfd07ed75a901493903c72053157b49c1f94c49
kassem00 <kamsdonga@gmail.com>
2023-02-27 16:42:18 +0200

    1

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * puts_half - function that prints every other character of a string
+ * @str: pointer to char
+ * Return: nothing
+ */
+
+void puts_half(char *str)
+{
+if(strlen(str) % 2 == 0)
+{
+size_t i = strlen(str) / 2;
+while (i < strlen(str))
+{
+_putchar(str[i]);
+i++
+}
+}else{
+/*odd*/
+}
+}
