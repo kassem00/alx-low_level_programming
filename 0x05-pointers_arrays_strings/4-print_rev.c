886921672c784c97e441cd619a0d83bd632f8976
kassem00 <kamsdonga@gmail.com>
2023-02-27 13:56:18 +0200

    1

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _puts - function that returns the length of a string.
+ * @str: pointer to char
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+
+int i = strlen(s);
+while (i >= 0)
+{
+i--;
+_putchar(s[i]);
+}
+_putchar('\n');
+}
