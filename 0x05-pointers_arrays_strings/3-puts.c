53c0ce0c4ceddf753ed220add258efb151486e8b
kassem00 <kamsdonga@gmail.com>
2023-02-27 12:11:24 +0200

    1

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _puts - function that returns the length of a string.
+ * @str: pointer to char
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+write(1, &c, 1);
+}
