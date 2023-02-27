229f40deb551b70eecd439810dbf75cc84638722
kassem00 <kamsdonga@gmail.com>
2023-02-27 15:38:14 +0200

    1

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * rev_string -  function that prints a string,
+ * in reverse, followed by a new line.
+ * @s: pointer to char
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+int i = strlen(s), j = 0;
+char temp[600];
+while (i > 0)
+{
+i--;
+temp[j] = s[i];
+j++;
+}
+while (i < j)
+{
+s[i] = temp[i];
+i++;
+}
+}
