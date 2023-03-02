84bc52af3e8f4c32f9179288072169453edbd9a3
kassem00 <kamsdonga@gmail.com>
2023-03-02 12:51:58 +0200

    1

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define TH_end '\0'
+/**
+ * *_strncat - function that concatenates two strings.
+ * @s1: pointer to char.
+ * @s2: pointer to char.
+ * Return: char.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+int u = 0;
+char temp= *s1;
+while (temp[u] != '\0')
+{
+temp[u] = *s1[u];
+u++;
+}
+s2 = &s2;
+return (dest);
+}
