6ecbc79e54c0806e54a7a9fa9dedef4edd261e62
kassem00 <kamsdonga@gmail.com>
2023-03-02 11:44:00 +0200

    1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -16,8 +16,10 @@ char *_strncat(char *dest, char *src, int n)
 int u = 0;
 char *temp = dest + strlen(dest);
 while (*src != '\0' && u < n)
+{
 *temp++ = *src++;
 u++;
+}
 *temp = TH_end;
 return (dest);
 }
