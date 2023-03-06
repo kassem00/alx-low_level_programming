9b2a199056c9f115b54c99f4a2056d1fe4d8f67d
kassem00 <kamsdonga@gmail.com>
2023-03-06 17:59:55 +0200

    1

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _memcpy - cpy
+ * _strchr - cpy
  * @c: input char
  * @s: input char
  * Return: char.
@@ -9,10 +9,10 @@
 char *_strchr(char *s, char c)
 {
 int i = 0;
-while(i >= '\0')
+while (i >= '\0')
 {
 i++;
-if(s[i] == c)
+if (s[i] == c)
 return (s + i);
 }
 return ('\0');
