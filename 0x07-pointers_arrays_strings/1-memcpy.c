6a41dc68dcc8715a3c00559c5d7bfcd661d4c2f0
kassem00 <kamsdonga@gmail.com>
2023-03-06 16:19:38 +0200

    1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,17 +4,15 @@
  * _memset - uppercase letters
  * @dest: input char
  * @src: input char
- * @n: input char
+ * @n: input unsigned int
  * Return: char.
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int i;
-for (i = 0; src[i] != '\0'; i++)
+for (i = 0;0 < n;i++, n--)
 {
-if (i < n)
-break;
 *(dest + i) = *(src + i);
 }
 return (dest);
