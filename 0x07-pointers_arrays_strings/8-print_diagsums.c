4c5a3b9bea99d469aad057625378603b20837642
kassem00 <kamsdonga@gmail.com>
2023-03-07 12:11:38 +0200

    1

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_diagnum1s - bt funcation.
+ * @a: pointer to char.
+ * @size: pointer to char.
+ * Return: void.
+ */
+
+void print_diagnum1s(int *a, int size)
+{
+int i = 0;
+int temp = 0;
+int num1, num12;
+num1 = num12 = 0;
+while (i < (size * size))
+{
+temp = *(a + i);
+num1 = num1 + temp;
+i = i + size + 1;
+}
+i = size - 1;
+while (i < ((size * size) - 1))
+{
+temp = *(a + i);
+num12 = num12 + temp;
+i = i + (size - 1);
+}
+printf("%d, %d\n", num1, num12);
+}
