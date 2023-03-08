d5afb4ef2a15b580d166df901153858b8df63ebf
kassem00 <kamsdonga@gmail.com>
2023-03-08 19:53:57 +0200

    1

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,9 +6,10 @@
  */
 int _strlen_recursion(char *s)
 {
-if(*s == '\0')
-return (0);
-else
-s++;
-return(_strlen_recursion(s));
+int x=0;
+if(*s != '\0')
+{
+x = _strlen_recursion(s + 1);
+}
+return(x);
 }
