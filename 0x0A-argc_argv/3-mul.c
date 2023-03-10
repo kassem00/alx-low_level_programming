34f107479cba9b06469de15b88740ddf4dfda287
kassem00 <kamsdonga@gmail.com>
2023-03-10 22:50:54 +0200

    1

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,35 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - funcation main aa
+ * main - funcation main
  * @argv: input char
  * @argc: input int
  * Return: 0.
  */
 int main(int argc, char **argv)
 {
-int counter, n1, n2, resualt;
-counter = n1 = n2 = resualt = 0;
+int count, num1, num2, res;
+count = num1 = num2 = res = 0;
 if (argc > 0)
 {
-while (counter < argc)
+while (count < argc)
 {
 if (argc == 3)
 {
-if (counter == 1)
-n1 = atoi(argv[counter]);
-else if (counter == 2)
-n2 = atoi(argv[counter]);
+if (count == 1)
+num1 = atoi(argv[count]);
+else if (count == 2)
+num2 = atoi(argv[count]);
 }
 else
 {
 printf("Error\n");
 return (1);
 }
-resualt = (n1 * n2);
-counter++;
+res = (num1 * num2);
+count++;
 }
-printf("%d\n", resualt);
+printf("%d\n", res);
 }
 return (0);
 }
