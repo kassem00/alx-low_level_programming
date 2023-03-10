417a21a98b11851fbbc6d483c9578802fed491e3
kassem00 <kamsdonga@gmail.com>
2023-03-10 22:07:49 +0200

    1

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - funcation main
+ * @argv: input char
+ * @argc: input int
+ * Return: 0.
+ */
+int main(int argc, char **argv)
+{
+while (argc--);
+printf("%d\n", atoi(*(argv + 1))*atoi(*(argv + 2)));
+exit(EXIT_SUCCESS);
+return (0);
+}
