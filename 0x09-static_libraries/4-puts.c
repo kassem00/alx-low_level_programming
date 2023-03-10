b3c3c1941637bc151573fa005cd6683299349efb
kassem00 <kamsdonga@gmail.com>
2023-03-10 17:57:52 +0200

    1

--- 0x09-static_libraries/4-puts.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-/**
- * Description: main-funcation {print text}
- * Return: 0 if it success.
- */
-int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle");
-return (0); }
---
+++ 0x09-static_libraries/we/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define TH_end '\0'
+/**
+ * *_strncat - function that concatenates two strings.
+ * @dest: pointer to char.
+ * @src: pointer to char.
+ * @n: number of charcter.
+ * Return: char.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+int u = 0;
+char *temp = dest + strlen(dest);
+while (*src != '\0' && u < n)
+{
+*temp++ = *src++;
+u++;
+}
+*temp = TH_end;
+return (dest);
+}
