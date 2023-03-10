3fc956603e3e2149c7f25ac91df82e376313cdf3
kassem00 <kamsdonga@gmail.com>
2023-03-10 20:32:48 +0200

    1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _memset - uppercase letters
+ * @s: input char
+ * @b: input char
+ * @n: input char
+ * Return: char.
+ */
+int main(int argc, char **argv)
+{
+printf("%s\n",*(argv + 0));
+return (0);
+}
