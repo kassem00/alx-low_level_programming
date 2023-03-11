b8057beec4864ad249d93b7486a4a001907a9d10
kassem00 <kamsdonga@gmail.com>
2023-03-11 15:52:51 +0200

    1

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -6,21 +6,28 @@
  * @argc: input int
  * Return: 0.
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv)
 {
-int flag=0,con = 1,num1, num2, res;
+int /*flag=0,*/con = 1,num1, num2, res;
+
 if (argc >= 1)
-printf("0\m");
+printf("0\n");
 else
 {
-while(con <= argc){
-if(isdigit(*(argv + con) != 1)
+while(con <= argc)
+{
+if(isdigit(*argv++) != 1)
+{
 printf("Error\n");
+}
 else
-num1 = atoi(argv[con]);
-num2 = atoi(argv[con+1]);
+{
+num1 = atoi(*argv[con]);
+num2 = atoi(*argv[con+1]);
 res = num1 + num2;
 printf("%d\n", res);
+}
+con++;
 }
 }
 return (0);
