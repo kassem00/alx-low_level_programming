0e086debeaa69e2b8a7eb8dd894ca04a0b9235f6
kassem00 <kamsdonga@gmail.com>
2023-03-13 22:41:37 +0200

    1

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,12 +14,12 @@ if (size == 0)
 ret = 1;
 else
 {
-array_of_char = malloc(sizeof(char) *size);
-if (*array_of_char == NULL)
+array_of_char = malloc(sizeof(char) * size);
+if (array_of_char == NULL)
 ret = 1;
 else
 {
-for (i = 0; i < size; i++)
+for (i = 0; i < size ;i++)
 array_of_char[i] = c;
 ret = 2;
 }
