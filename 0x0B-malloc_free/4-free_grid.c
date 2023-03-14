9cb408688d15cb78ef15dc4f7a114e0c95570c9d
kassem00 <kamsdonga@gmail.com>
2023-03-14 23:17:38 +0200

    1

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * free_grid - immpalimante funcation free alloceted array
+ * @height: height of the grid
+ * Return: void.
+ */
+void free_grid(int **grid, int height)
+{
+int i = 0;
+while(i < height)
+{
+free(grid[i]);
+i++
+}
+free(grid);
+}
