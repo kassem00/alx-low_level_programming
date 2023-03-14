fc9dc677359c3c4f62cbebefcfb4f0547b048644
kassem00 <kamsdonga@gmail.com>
2023-03-14 19:30:20 +0200

    1

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * str_concat - immpalimante funcation array
+ * @s1: the char.
+ * @s2: the char.
+ * Return: char.
+ */
+int **alloc_grid(int width, int height);
+{
+int i, j, *ptr;
+if (width <= 0 || height <= 0)
+return (NULL);
+else
+{
+ptr = malloc((height * width) * sizeof(int));
+for (int i = 0; i < width * height; i++)
+ptr[i] = 0;
+return (*ptr);
+}
