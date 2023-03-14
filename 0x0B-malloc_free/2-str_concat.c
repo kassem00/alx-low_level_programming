952d0ac49fb4d23b7b0ccc739626e5687ea7e6ff
kassem00 <kamsdonga@gmail.com>
2023-03-14 18:09:39 +0200

    1

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,10 +16,16 @@ j++;
 array_of_char = malloc((sizeof(char) * (i + j + 1)));
 if (array_of_char == NULL)
 return (NULL);
+if (i > 0)
+{
 for (i = 0; s1[i] != '\0'; i++)
 array_of_char[i] = s1[i];
+}
+if (j > 0)
+{
 for (j = 0; s2[j] != '\0'; j++ ,i++)
 array_of_char[i] = s2[j];
 array_of_char[i] = '\0';
+}
 return (array_of_char);
 }
