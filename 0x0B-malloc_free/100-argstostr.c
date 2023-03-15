4dbc1a09894b7d8bca70213063d1445b9e1982ea
kassem00 <kamsdonga@gmail.com>
2023-03-15 22:36:41 +0200

    1

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -16,16 +16,18 @@ for (i = 0; i<ac; i++)
 for(x = 0; av[i][x] != '\0'; x++);
 count = count + x;
 }
-ptr_arr_char = malloc(sizeof(char) * count);
+ptr_arr_char = malloc((sizeof(char) * count) + ac);
 if (ptr_arr_char == NULL)
 return (NULL);
 for (i = 0; i<ac; i++)
 {
 for(x = 0; av[i][x] != '\0'; x++)
 {
-ptr_arr_char = av[i][x];
+*(ptr_arr_char + count) = av[i][x];
+count--;
 }
-ptr_arr_char = '\n';
+*(ptr_arr_char + count) = '\n';
+count--;
 }
 ptr_arr_char = '\0';
 return(ptr_arr_char);
