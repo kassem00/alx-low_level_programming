0c3b3d9a28c3d317896b5dfdea4d381007d061c2
kassem00 <kamsdonga@gmail.com>
2023-03-15 22:04:13 +0200

    1

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -7,26 +7,11 @@
  */
 char *argstostr(int ac, char **av)
 {
-int **arra;
-int row, col;
-if (width <= 0 || height <= 0)
-return (NULL);100-argstostr.c
-arra = malloc(sizeof(int *) * height);
-if (arra == NULL)
+int i;
+if (ac == 0 || av == NULL)
 return (NULL);
-for (row = 0; row < height; row++)
+for (i = 0; i<ac; i++)
 {
-arra[row] = malloc(sizeof(int) * width);
-if (arra[row] == NULL)
-{
-for (; row >= 0; row--)
-free(arra[row]);
-free(arra);
-return (NULL);
+
 }
-}
-for (row = 0; row < height; row++)
-for (col = 0; col < width; col++)
-arra[row][col] = 0;
-return (arra);
 }
