7a6fa9b40ff5fb0aba0c0ce40c2fb333c64ab6c6
kassem00 <kamsdonga@gmail.com>
2023-03-15 22:20:04 +0200

    1

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -7,11 +7,14 @@
  */
 char *argstostr(int ac, char **av)
 {
-int i;
+int i, x,count = 0;
+char *ptr_arr_char;
 if (ac == 0 || av == NULL)
 return (NULL);
 for (i = 0; i<ac; i++)
 {
-
+for(x = 0; av[i][x] != '\0'; x++);
+count = count + x;
 }
+ptr_arr_char = malloc(sizeof(char) * count);
 }
