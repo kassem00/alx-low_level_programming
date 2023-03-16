cc447c7fd3252a0fcace9f9d2057c0a1178246dd
kassem00 <kamsdonga@gmail.com>
2023-03-16 21:09:01 +0200

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,8 +2,8 @@
 /**
  * string_nconcat -  function that allocates memory using
  * @n: unsigned int.
- * @n: input char.
- * @n: input char.
+ * @s1: input char.
+ * @s2: input char.
  * Return: pointer to char.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
