2fc68232165f32f72f75f82e4f0acd76421bf815
kassem00 <kamsdonga@gmail.com>
2023-03-16 20:48:42 +0200

    1

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -8,10 +8,28 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-void *array_of;
-array_of = malloc(b);
-if (array_of == NULL)
-exit(98);
+char *s;
+unsigned int i = 0, j = 0, string_leng1 = 0, string_leng2 = 0;
+while (s1 && s1[string_leng1])
+string_leng1++;
+while (s2 && s2[string_leng2])
+string_leng2++;
+if (n < string_leng2)
+s = malloc(sizeof(char) * (string_leng1 + n + 1));
 else
-return (array_of);
+s = malloc(sizeof(char) * (string_leng1 + string_leng2 + 1));
+if (!s)
+return (NULL);
+while (i < string_leng1)
+{
+s[i] = s1[i];
+i++;
+}
+while (n < string_leng2 && i < (string_leng1 + n))
+s[i++] = s2[j++];
+while (n >= string_leng2 && i < (string_leng1 + string_leng2))
+s[i++] = s2[j++];
+s[i] = '\0';
+return (s);
+}
 }
