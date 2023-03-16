6d836707f0fbcec98f95a613e98d501d2e06163c
kassem00 <kamsdonga@gmail.com>
2023-03-16 21:37:04 +0200

    1

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -5,7 +5,7 @@
  * @size: unsigned int.
  * Return: pointer.
  */
-void *_calloc(unsigned int nmemb, unsigned int size);
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
 char *ptr_to_char;
 if (nmemb == 0|| size == 0)
@@ -15,4 +15,23 @@ if (ptr_to_char == NULL)
 return (NULL);
 _memset(ptr_to_char, 0, nmemb * size);
 return (ptr_to_char);
+}
+/**
+ * _memset - uppercase letters
+ * @s: input char
+ * @b: input char
+ * @n: input char
+ * Return: char.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i = 0;
+char *re = s;
+while (i < n)
+{
+s[i] = b;
+i++;
+}
+return (re);
 }
