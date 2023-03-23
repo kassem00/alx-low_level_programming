6bb03b51fa9b00de84e677701e6edebc3637f838
kassem00 <kamsdonga@gmail.com>
2023-03-23 02:57:05 +0200

    1

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,11 +3,13 @@
 /**
  * print_name - prints a name
  * @name: name of the person
- * @f - pointer to funcation 
+ * @f - pointer to funcation
  *
  * Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *))
 {
+if (name == NULL || f == NULL)
+return;
 f(name);
 }
