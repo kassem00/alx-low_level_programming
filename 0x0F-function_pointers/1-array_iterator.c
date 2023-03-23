6b50156974da82d482f523a646dce20b48ba6761
kassem00 <kamsdonga@gmail.com>
2023-03-23 03:50:48 +0200

    1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - prints a name
+ * @array: name of the person
+ * @size: pointer to funcation
+ * @action: pointer to funcation
+ * Return: void.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int));
+{
+size_t i;
+if (size == NULL || array == NULL || action == NULL)
+return;
+for(i = 0; i < size; i++)
+action(*(array + i));
+}
