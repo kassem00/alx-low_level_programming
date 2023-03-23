a5cd02edfc7c11cb21afc595aec5a9fe14bd1d39
kassem00 <kamsdonga@gmail.com>
2023-03-23 04:42:21 +0200

    1

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -4,7 +4,7 @@
  * int_index - index funcation
  * @array: name of the person
  * @size: size of array
- * @mcp: pointer to funcation
+ * @cmp: pointer to funcation
  * Return: void.
  */
 int int_index(int *array, int size, int (*cmp)(int))
