60e8f0f458828ee6f8401c5c0904f33d23dc9136
kassem00 <kamsdonga@gmail.com>
2023-03-23 02:56:05 +0200

    1

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,8 +1,9 @@
 #include "function_pointers.h"
 
 /**
- * print_name_as_is - prints a name as is
+ * print_name - prints a name
  * @name: name of the person
+ * @f - pointer to funcation 
  *
  * Return: Nothing.
  */
