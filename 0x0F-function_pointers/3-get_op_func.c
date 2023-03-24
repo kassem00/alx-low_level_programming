11791932ee4a7521b23c222883c61cbef6c1ab04
kassem00 <kamsdonga@gmail.com>
2023-03-24 05:07:12 +0200

    1

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -10,7 +10,6 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-int i = 0;
 op_t ops[] = {
 {"+", op_add},
 {"-", op_sub},
@@ -19,6 +18,7 @@ op_t ops[] = {
 {"%", op_mod},
 {NULL, NULL},
 };
+int i = 0;
 while (ops[i].op != NULL && *(ops[i].op) != *s)
 i++;
 return (ops[i].f);
