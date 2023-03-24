c326e5183b076cf3242f65018275d645852b5345
kassem00 <kamsdonga@gmail.com>
2023-03-24 05:14:37 +0200

    1

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,6 +3,7 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include "3-calc.h"
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
