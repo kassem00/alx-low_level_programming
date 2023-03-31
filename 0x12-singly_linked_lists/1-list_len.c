2fa78b8849266c1df30a2b75a813fe24584b7b63
kassem00 <kamsdonga@gmail.com>
2023-03-31 04:50:23 +0200

    1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,14 +1,14 @@
 #include "lists.h"
 
 /**
- * list_len - funcation return number of elements
- * @h: pointer .
- * Return: size_t .
+ * list_len - funcation return number of elements.
+ * @h: pointer.
+ * Return: size_t.
  */
 size_t list_len(const list_t *h)
 {
 size_t num_nodes = 0;
-for(;h != NULL; num_nodes++)
+for(; h != NULL; num_nodes++)
 { 
 num_nodes++;
 h = h->next;
