1ba4b97d79becbbbda0ac1500d151d5baea8837a
kassem00 <kamsdonga@gmail.com>
2023-03-31 04:49:59 +0200

    1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * list_len - funcation return number of elements
+ * @h: pointer .
+ * Return: size_t .
+ */
+size_t list_len(const list_t *h)
+{
+size_t num_nodes = 0;
+for(;h != NULL; num_nodes++)
+{ 
+num_nodes++;
+h = h->next;
+}
+return (num_nodes);
+}
