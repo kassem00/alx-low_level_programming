8a3fade9356d495c0ce339b7f7398e83b57bd834
kassem00 <kamsdonga@gmail.com>
2023-03-31 22:39:56 +0200

    1

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,4 @@
-#include <stdlib.h>
-#include <string.h>
-#include "lists.h"
+	#include "lists.h"
 
 /**
  * add_node - adds a new node .
@@ -15,10 +13,10 @@ unsigned int in_length = 0;
 while (*(str + in_length))
 in_length++;
 n_node = malloc(sizeof(list_t));
-if (!n_node)
+if (n_node == NULL)
 return (NULL);
 n_node->str = strdup(str);
-n_node->in_length = in_length;
+n_node->len = in_length;
 n_node->next = (*head);
 (*head) = n_node;
 return (*head);
