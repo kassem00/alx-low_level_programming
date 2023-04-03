1daa4d4cca2fdb6018366883bcf62f0795ecef83
kassem00 <kamsdonga@gmail.com>
2023-04-03 17:15:57 +0200

    1

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,16 +9,16 @@
 int main(void)
 {
 int n1, n2, n3;
-for(n1 = 0; n1 <= 9; n1++)
+for (n1 = 0; n1 <= 9; n1++)
 {
-for(n2 = n1+1; n2 <= 9; n2++)
+for (n2 = n1+1; n2 <= 9; n2++)
 {
-for(n3 = n2+1; n3 <= 9; n3++)
+for (n3 = n2+1; n3 <= 9; n3++)
 {
 putchar(n1 + '0');
 putchar(n2 + '0');
 putchar(n3 + '0');
-if(n1 != 7 || n2 != 8 || n3 != 9)
+if (n1 != 7 || n2 != 8 || n3 != 9)
 {
 putchar(',');
 putchar(' ');
