8c4d7d688c42962d028de84a765d4757734482b3
kassem00 <kamsdonga@gmail.com>
2023-04-03 17:15:03 +0200

    1

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,23 +6,20 @@
 * followed by a new line.
 * Return: 0
 */
-int main(void) {
-int i, j, k;
-for (i = 0; i < 8; i++) {
-for (j = i + 1; j < 9; j++) {
-for (k = j + 1; k < 10; k++) {
-if ((i == 0 && j == 1 && k == 2) ||
-(i == 1 && j == 0 && k == 2) ||
-(i == 2 && j == 1 && k == 0) ||
-(i == 0 && j == 2 && k == 1) ||
-(i == 1 && j == 2 && k == 0) ||
-(i == 2 && j == 0 && k == 1)) {
-continue;
-}
-putchar(i + '0');
-putchar(j + '0');
-putchar(k + '0');
-if (i != 7 || j != 8 || k != 9) {
+int main(void)
+{
+int n1, n2, n3;
+for(n1 = 0; n1 <= 9; n1++)
+{
+for(n2 = n1+1; n2 <= 9; n2++)
+{
+for(n3 = n2+1; n3 <= 9; n3++)
+{
+putchar(n1 + '0');
+putchar(n2 + '0');
+putchar(n3 + '0');
+if(n1 != 7 || n2 != 8 || n3 != 9)
+{
 putchar(',');
 putchar(' ');
 }
