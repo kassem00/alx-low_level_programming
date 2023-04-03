fda59e9737021bcafe30147a6c7f88290853d842
kassem00 <kamsdonga@gmail.com>
2023-04-03 17:03:14 +0200

    1

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,13 +6,16 @@
 * followed by a new line.
 * Return: 0
 */
-int main() {
+int main(void) {
 int i, j;
-for (i = 0; i < 9; i++) {
-for (j = i + 1; j < 10; j++) {
+for (i = 0; i < 9; i++)
+{
+for (j = i + 1; j < 10; j++)
+{
 putchar(i + '0');
 putchar(j + '0');
-if (i != 8 || j != 9) {
+if (i != 8 || j != 9)
+{
 putchar(',');
 putchar(' ');
 }
