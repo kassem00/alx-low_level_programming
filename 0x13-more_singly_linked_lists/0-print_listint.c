bfbd4ad93c7547c4f91bc80b28ce60f860fad64f
kassem00 <kamsdonga@gmail.com>
2023-04-04 08:23:03 +0200

    1

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_listint - prints linked list.
+ * @h: pointer .
+ * Return: size_t.
+ */
+
+size_t print_listint(const listint_t *h)
+{
+size_t num_nodes = 0;
+while (h)
+{
+if (h->next)
+{
+printf("%d\n", h->n);
+}
+h = h->next;
+num_nodes++;
+}
+return (num_nodes);
+}
