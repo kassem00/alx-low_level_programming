b2be2bf6142c8445771539ef0976682b2d9d09c9
kassem00 <kamsdonga@gmail.com>
2023-04-05 05:17:57 +0200

    1

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index - returns the node at 
+ * get_nodeint_at_index - returns the node at
  * a certain index in a linked list
  * @head: first node in the linked list
  * @index: index of the node to return
@@ -13,5 +13,5 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 listint_t *temp = head;
 while (temp && index--)
 temp = temp->next;
-return temp;
+return (temp);
 }
