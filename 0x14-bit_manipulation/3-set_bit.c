693ef5166040f67cdbce1bef93a318086c9f8b88
kassem00 <kamsdonga@gmail.com>
2023-04-09 15:03:34 +0200

    1

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,13 +9,11 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-    unsigned long int mask;
-    (void) index;
-
-    mask = 1UL << index; /* create a mask with a 1 at the index position */
-    if (index > 63) /* make sure index is within bounds */
-        return (-1);
-
-    *n |= mask; /* set the bit to 1 */
-    return (1);
+unsigned long int mask;
+(void) index;
+mask = 1UL << index; /* create a mask with a 1 at the index position */
+if (index > 63) /* make sure index is within bounds */
+return (-1);
+*n |= mask; /* set the bit to 1 */
+return (1);
 }
