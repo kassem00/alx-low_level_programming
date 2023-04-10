e9c9f1bca2b99669f2bfe3587e1abfc01a1bc3ce
kassem00 <kamsdonga@gmail.com>
2023-04-10 23:52:13 +0200

    1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * read_textfile - reads a text file and writes its contents to the standard
  * output
@@ -11,42 +10,35 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t num_read, num_written;
-    char *buffer;
-
-    if (filename == NULL)
-        return (0);
-
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
-        return (0);
-
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-    {
-        free(buffer);
-        return (0);
-    }
-
-    num_read = read(fd, buffer, letters);
-    if (num_read == -1)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
-
-    num_written = write(STDOUT_FILENO, buffer, num_read);
-    if (num_written == -1 || num_written != num_read)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
-
-    free(buffer);
-    close(fd);
-
-    return (num_written);
+int fd;
+ssize_t num_read, num_written;
+char *buffer;
+if (filename == NULL)
+return (0);
+buffer = malloc(sizeof(char) * letters);
+if (buffer == NULL)
+return (0);
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+{
+free(buffer);
+return (0);
+}
+num_read = read(fd, buffer, letters);
+if (num_read == -1)
+{
+free(buffer);
+close(fd);
+return (0);
+}
+num_written = write(STDOUT_FILENO, buffer, num_read);
+if (num_written == -1 || num_written != num_read)
+{
+free(buffer);
+close(fd);
+return (0);
+}
+free(buffer);
+close(fd);
+return (num_written);
 }
