adf470985118d1416205c6eb55cd0c25edd3c16c
kassem00 <kamsdonga@gmail.com>
2023-04-10 22:13:00 +0200

    1

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * read_textfile - function that reads a text file 
+ * and prints it to the POSIX standard output.
+ * @filename: input char
+ * @letters: input size_t
+ * Return: return.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+}
---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * read_textfile - function that reads a text file 
+ * and prints it to the POSIX standard output.
+ * @filename: input char
+ * @letters: input size_t
+ * Return: return.
+ */
+int create_file(const char *filename, char *text_content)
+{
+
+}
