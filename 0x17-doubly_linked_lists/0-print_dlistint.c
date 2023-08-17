82898b660992a2fa136ad751b1846def8242d0e5
kassem00 <kamsdonga@gmail.com>
2023-08-18 02:34:47 +0300

    1

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -16,10 +16,12 @@ size_t print_dlistint(const dlistint_t *h)
 
 		while (1)
 		{
-			i++;
 			printf("%d\n", temp->n);
 			if (temp->next != NULL)
+			{
 				temp = temp->next;
+				i++;
+			}
 			else
 				return (i);
 		}
