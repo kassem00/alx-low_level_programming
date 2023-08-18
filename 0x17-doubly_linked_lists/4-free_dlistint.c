3196b3480ca939a00ed5928637f9a5d0418626ad
kassem00 <kamsdonga@gmail.com>
2023-08-18 04:39:44 +0300

    1

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - function that add node in head
+ * @head:linked list node
+ * @n:data to store in new node
+ * Return: size_t which number of node
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
