d288777294a3945900041f00d8667809c9522814
kassem00 <kamsdonga@gmail.com>
2023-09-07 19:15:22 +0300

    1

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,14 +7,14 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *p = malloc(sizeof(hash_table_t));
-	hash_node_t *t_head = malloc(sizeof(hash_node));
+	hash_node_t *t_head = malloc(sizeof(hash_node_t));
 	int i;
 
-	p->size = 1024;
-	for (i = 0; i < SIZE_OF_TABLE; i++)
+	p->size = size;
+	for (i = 0; i < (int)size; i++)
 	{
 		t_head = t_head->next;
-		t_head->next = malloc(sizeof(hash_node));
+		t_head->next = malloc(sizeof(hash_node_t));
 	}
-	return (p)
+	return (p);
 }
