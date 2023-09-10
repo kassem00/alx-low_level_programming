37ba4259029308a505b5929ee66ae5a8e58dc308
kassem00 <kamsdonga@gmail.com>
2023-09-10 19:56:42 +0000

    1

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,23 +11,22 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *temp;
 
-	if (key != NULL || ht != NULL)
+	if (key == NULL || ht == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	if (ht->array[index] != NULL && strcmp(ht->array[index]->key,
+					       key) == 0)
 	{
-		index = key_index((const unsigned char *)key, ht->size);
-		if (ht->array[index] != NULL && strcmp(ht->array[index]->key,
-						       key) == 0)
-		{
-			ht->array[index]->value = strdup(value);
-			return (1);
-		}
-		temp = malloc(sizeof(hash_node_t));
-		if (temp == NULL)
-			return (0);
-		temp->key = strdup(key);
-		temp->value = strdup(value);
-		temp->next = ht->array[index];
-		ht->array[index] = temp;
+		ht->array[index]->value = strdup(value);
 		return (1);
 	}
-	return (0);
+	temp = malloc(sizeof(hash_node_t));
+	if (temp == NULL)
+		return (0);
+	temp->key = strdup(key);
+	temp->value = strdup(value);
+	temp->next = ht->array[index];
+	ht->array[index] = temp;
+	return (1);
 }
