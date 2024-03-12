71740b7a128f22949065e8eb057508f4bcb311a0
kassem00 <kamsdonga@gmail.com>
2024-03-12 04:08:51 +0000

    au

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -7,19 +7,22 @@
  * @value: element to found in array
  * Return: -1 on error else number found
  */
+
 int linear_search(int *array, size_t size, int value)
 {
 	int i;
+
 	if (array == NULL)
 	{
 		return (ERROR);
 	}
-        for (i = 0; i < (int) size; i++)
+
+	for (i = 0; i < (int)size; i++)
 	{
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 
 		if (array[i] == value)
 			return (value);
-        }
+	}
 	return (ERROR);
 }
