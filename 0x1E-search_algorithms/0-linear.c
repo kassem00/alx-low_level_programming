8eea225779fba35f5a93327fc79047b67d4c739a
kassem00 <kamsdonga@gmail.com>
2024-03-12 04:13:44 +0000

    au

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -22,7 +22,7 @@ int linear_search(int *array, size_t size, int value)
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 
 		if (array[i] == value)
-			return (value);
+			return (i);
 	}
 	return (ERROR);
 }
