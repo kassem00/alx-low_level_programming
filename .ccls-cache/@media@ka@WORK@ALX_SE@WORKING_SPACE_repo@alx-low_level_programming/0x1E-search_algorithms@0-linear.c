599e7cea3e8ca925707fa09387092bb3dd25e3f4
kassem00 <kamsdonga@gmail.com>
2024-03-12 04:15:33 +0000

    au

---
+++ .ccls-cache/@media@ka@WORK@ALX_SE@WORKING_SPACE_repo@alx-low_level_programming/0x1E-search_algorithms@0-linear.c
@@ -0,0 +1,28 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - linear search function
+ * @array: pointer to first element in array
+ * @size: array size
+ * @value: element to found in array
+ * Return: -1 on error else number found
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	int i;
+
+	if (array == NULL)
+	{
+		return (ERROR);
+	}
+
+	for (i = 0; i < (int)size; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+
+		if (array[i] == value)
+			return (i);
+	}
+	return (ERROR);
+}
