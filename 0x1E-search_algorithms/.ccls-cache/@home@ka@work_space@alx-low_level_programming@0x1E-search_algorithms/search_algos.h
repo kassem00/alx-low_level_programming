15ce4ba1e819b5f65991615ae043f6adb173dc07
kassem00 <kamsdonga@gmail.com>
2024-05-10 13:05:52 +0000

    au

---
+++ 0x1E-search_algorithms/.ccls-cache/@home@ka@work_space@alx-low_level_programming@0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#define ERROR -1
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+void insertion_sort(int *array, size_t size);
+#endif
