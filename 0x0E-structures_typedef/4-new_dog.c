f1438614e442a47c85d9d71ff6f4771e4b9264dd
kassem00 <kamsdonga@gmail.com>
2025-07-11 01:03:07 +0300

    au

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,7 +16,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	char *owner_copy;
 	int name_len = 0, owner_len = 0, i;
 
-	/* Calculate lengths of name and owner */
 	if (name != NULL)
 		while (name[name_len])
 			name_len++;
@@ -24,23 +23,19 @@ dog_t *new_dog(char *name, float age, char *owner)
 		while (owner[owner_len])
 			owner_len++;
 
-	/* Allocate memory for the dog structure */
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
 
-	/* Allocate memory for name copy */
 	name_copy = malloc(name_len + 1);
 	if (name_copy == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
-	/* Copy name */
 	for (i = 0; i <= name_len; i++)
 		name_copy[i] = name ? name[i] : '\0';
 
-	/* Allocate memory for owner copy */
 	owner_copy = malloc(owner_len + 1);
 	if (owner_copy == NULL)
 	{
@@ -48,11 +43,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(new_dog);
 		return (NULL);
 	}
-	/* Copy owner */
 	for (i = 0; i <= owner_len; i++)
 		owner_copy[i] = owner ? owner[i] : '\0';
 
-	/* Initialize the dog structure */
 	new_dog->name = name_copy;
 	new_dog->age = age;
 	new_dog->owner = owner_copy;
