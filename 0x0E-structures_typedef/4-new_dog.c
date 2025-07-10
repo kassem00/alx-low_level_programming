bd49ef8144aef113ddcad5cbb94ca568f8219c30
kassem00 <kamsdonga@gmail.com>
2025-07-11 01:00:10 +0300

    au

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
 
 /**
@@ -15,42 +14,43 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *new_dog;
 	char *name_copy;
 	char *owner_copy;
+	int name_len = 0, owner_len = 0, i;
+
+	/* Calculate lengths of name and owner */
+	if (name != NULL)
+		while (name[name_len])
+			name_len++;
+	if (owner != NULL)
+		while (owner[owner_len])
+			owner_len++;
 
 	/* Allocate memory for the dog structure */
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
 
-	/* Allocate and copy name */
-	if (name != NULL)
+	/* Allocate memory for name copy */
+	name_copy = malloc(name_len + 1);
+	if (name_copy == NULL)
 	{
-		name_copy = strdup(name);
-		if (name_copy == NULL)
-		{
-			free(new_dog);
-			return (NULL);
-		}
+		free(new_dog);
+		return (NULL);
 	}
-	else
-	{
-		name_copy = NULL;
-	}
+	/* Copy name */
+	for (i = 0; i <= name_len; i++)
+		name_copy[i] = name ? name[i] : '\0';
 
-	/* Allocate and copy owner */
-	if (owner != NULL)
+	/* Allocate memory for owner copy */
+	owner_copy = malloc(owner_len + 1);
+	if (owner_copy == NULL)
 	{
-		owner_copy = strdup(owner);
-		if (owner_copy == NULL)
-		{
-			free(name_copy);
-			free(new_dog);
-			return (NULL);
-		}
+		free(name_copy);
+		free(new_dog);
+		return (NULL);
 	}
-	else
-	{
-		owner_copy = NULL;
-	}
+	/* Copy owner */
+	for (i = 0; i <= owner_len; i++)
+		owner_copy[i] = owner ? owner[i] : '\0';
 
 	/* Initialize the dog structure */
 	new_dog->name = name_copy;
